d8760a5509f1a8106abbe1275be4174cf6d9d041
Ashith Shetty <ashitdshetty1101@gmail.com>
2023-02-09 21:52:04 +0530

    made features section responsive

--- src/components/Features.js
+++ src/components/Features.js
@@ -8,23 +8,24 @@ import content from "../Photo/content.jpg"
 function Features() {
   return (
     <>
-        <div className='ft-textslide'>
-                <div className="ft-photo">
-                  <SlideImage image={photographer} title="Photographer" />
-                </div>
-                <div className="ft-edit">
-                  <SlideImage image={videography} title="Videography" />
-                </div>
-                <div className="ft-video">
-                  <SlideImage image={editing} title="Editing" />
-                </div>
-                <div className="ft-content">
-                  <SlideImage image={content} title="Content"/>
-                </div>
-                <div className='ft'>
-                  <h3 className='ft-clubdesc'>The Photography, Videography & Editing Club of IIIT Dharwad.</h3>
-                </div>
+      <div className='ft-textslide'>
+        <div className='ft'>
+          <h3 className='ft-clubdesc'>The Photography, Videography & Editing Club of IIIT Dharwad.</h3>
         </div>
+        <div className="ft-photo">
+          <SlideImage image={photographer} title="Photographer" />
+        </div>
+        <div className="ft-video">
+          <SlideImage image={videography} title="Videography" />
+        </div>
+        <div className="ft-edit">
+          <SlideImage image={editing} title="Editing" />
+        </div>
+        <div className="ft-content">
+          <SlideImage image={content} title="Content" />
+        </div>
+
+      </div>
     </>
   )
 }
