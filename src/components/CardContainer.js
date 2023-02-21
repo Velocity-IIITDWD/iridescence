b029cacd09673466cba6b7e7b90b3e4317d78fdf
Mandy767 <92856810+Mandy767@users.noreply.github.com>
2023-02-21 15:15:10 +0530

    added images of few members

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -14,7 +14,15 @@ import Priyansh from "../images/Priyansh.JPG"
 import Anand from "../images/Anand.JPG"
 import Varang from "../images/Varang.JPG"
 import Jatin from "../images/Jatin.JPG"
-
+import Shreyansh from "../images/Shreyansh.JPG"
+import Chinmay_Shewale from "../images/Chinmay_Shewale.JPG"
+import vansh from "../images/Vansh.JPG"
+import Lohith from "../images/Lohith.JPG"
+import Aryan_jaiswal from "../images/Aryan_Jaiswal.JPG"
+import Vinay_Jain from "../images/Vinay_Jain.JPG"
+import Ashitosh from "../images/Ashitosh.JPG"
+import Harikiran from "../images/Harikiran.JPG"
+import Aryan_TN from "../images/Aryan_TN.JPG"
 
 
 const CardContainer= () => {
@@ -53,9 +61,9 @@ const CardContainer= () => {
 
           <div className="row">
             <MainCard name="Ratnesh Kherudkar" role="President" email="21bcs091@iiitdwd.ac.in" />
-            <MainCard name="Chinmay Shewale" role="Editing Head" email="21bcs108@iiitdwd.ac.in" />
+            <MainCard name="Chinmay Shewale" role="Editing Head" email="21bcs108@iiitdwd.ac.in" image={Chinmay_Shewale} />
             <MainCard name="Mandar Dighe" role="Member" email="21bcs033@iiitdwd.ac.in" image={Mandar}/>
-            <MainCard name="Ashitosh Sable" role="Member" email="21bcs096@iiitdwd.ac.in" />
+            <MainCard name="Ashitosh Sable" role="Member" email="21bcs096@iiitdwd.ac.in" image={Ashitosh}/>
 
 
 
@@ -64,16 +72,16 @@ const CardContainer= () => {
 
           <div className="row">
             
-          <MainCard name="Lohith B" role="Member" email="22bcs061@iiitdwd.ac.in" />
+          <MainCard name="Lohith B" role="Member" email="22bcs061@iiitdwd.ac.in" image={Lohith}/>
             <MainCard name="Jatin Kushwaha" role="Member" email="22bds030@iiitdwd.ac.in" image={Jatin} />
-            <MainCard name="Harikiran" role="Member" email="22bcs060@iiitdwd.ac.in" />
+            <MainCard name="Harikiran" role="Member" email="22bcs060@iiitdwd.ac.in" image={Harikiran}/>
             <MainCard name="Sreehitha" role="Member" email="22bcs050@iiitdwd.ac.in" />
           </div>
 
           <h1>Photography & Videography</h1>
           <div className="row">
             <MainCard name="Ashith Shetty" role="Vice President" email="21bcs019@iiitdwd.ac.in" image={Ashith_Shetty} />
-            <MainCard name="Aryan TN" role="Member" email="21bds006@iiitdwd.ac.in"/>
+            <MainCard name="Aryan TN" role="Member" email="21bds006@iiitdwd.ac.in" image={Aryan_TN}/>
             <MainCard name="Anand Singh" role="Member" email="21bcs009@iiitdwd.ac.in" image={Anand}/>
             <MainCard name="Ashish Lal" role="Member" email="21bcs017@iiitdwd.ac.in" />
 
@@ -90,7 +98,7 @@ const CardContainer= () => {
           <div className="row">
           <MainCard name="Priyansh" role="Member" email="22bcs092@iiitdwd.ac.in" image={Priyansh}/>
             <MainCard name="Varang Pratap Singh" role="Member" email="22bds062@iiitdwd.ac.in" image={Varang}/>
-            <MainCard name="Vansh Tolani" role="Member" email="22bds061@iiitdwd.ac.in" />
+            <MainCard name="Vansh Tolani" role="Member" email="22bds061@iiitdwd.ac.in" image={vansh} />
             <MainCard name="Somil Jain" role="Member" email="22bec045@iiitdwd.ac.in" image={Somil}/>
           </div>
 
@@ -99,7 +107,7 @@ const CardContainer= () => {
           <div className="row">
             <MainCard name="Chaytali Jawalekar" role="Content Head" email="21bcs026@iiitdwd.ac.in"/>
             <MainCard name="Ayushi Sinha" role="Member" email="21bcs120@iiitdwd.ac.in"/>
-            <MainCard name="Shreyansh Tiwari" role="Member" email="21bcs114@iiitdwd.ac.in"/>
+            <MainCard name="Shreyansh Tiwari" role="Member" email="21bcs114@iiitdwd.ac.in" image={Shreyansh}/>
          
 
             <MainCard name="Nomtha Prakash" role="Member" email="22bcs103@iiitdwd.ac.in"/>
@@ -118,14 +126,14 @@ const CardContainer= () => {
             <MainCard name="Chintan Chawda" role="Member" email="21bds013@iiitdwd.ac.in"/>
 
             <MainCard name="K Meghana" role="Member" email="22bcs054@iiitdwd.ac.in"/>
-            <MainCard name="Vinay Jain" role="Member" email="22bcs137@iiitdwd.ac.in"/>
+            <MainCard name="Vinay Jain" role="Member" email="22bcs137@iiitdwd.ac.in" image={Vinay_Jain}/>
 
 
           </div>
 
           <div className="row">
           <MainCard name="T Sameera" role="Member" email="22bec047@iiitdwd.ac.in"/>
-            <MainCard name="Aryan Jaiswal" role="Member" email="@iiitdwd.ac.in"/>
+            <MainCard name="Aryan Jaiswal" role="Member" email="@iiitdwd.ac.in" image={Aryan_jaiswal}/>
 
           </div>
 
