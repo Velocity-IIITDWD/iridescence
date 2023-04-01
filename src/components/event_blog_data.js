b6603a9bc02048be83c6ec558bc1acf39f61887b
Ashith Shetty <ashitdshetty1101@gmail.com>
2023-04-01 23:30:53 +0530

    update in work page data

--- src/components/event_blog_data.js
+++ src/components/event_blog_data.js
@@ -1,17 +1,38 @@
+import img1 from "../Work Photo/Screenshot 2023-04-01 231533.png";
+import img2 from "../Work Photo/DSC_6265.JPG"
+import img3 from "../Work Photo/DSC_0066.JPG"
+import img4 from "../Work Photo/DSC_3172.JPG"
+import img5 from "../Work Photo/IMG_0430.JPG"
+import img6 from "../Work Photo/DSC_0112.JPG"
 export const event_blog_data = [
     {
-        image: "https://iiitdwd.ac.in/img/home-page/fit.jpg",
-        caption: "Teacher's Day",
-        event_caption: "You have always been an excellent educator who knew how to illuminate a soul with its light. Happy Teacher's Day to my favorite teacher!"
+        image: img1,
+        caption: "Lensation",
+        event_caption: "Iridescence holds a monthly contest for all students to submit their clicked photographs on a given theme. The best ones are posted on our Instagram handle and published in the college magazine. With unique themes, the contest provides a great opportunity for everyone to showcase their creativity and imagination."
     },
     {
-        image: "https://pbs.twimg.com/media/EIR78qFW4AAS6ZZ.jpg:large",
-        caption: "Tech Fest",
-        event_caption: "There's no room for lone wolves. The strength of the pack is the wolf. The strength of the wolf is the pack."
+        image: img2,
+        caption: "Smt.Sudha Murthy Mam's visit",
+        event_caption: "The Chairperson of IIIT Dharwad, on her visit to the campus, sat down among the students to have a warm conversation. Being a Philanthropist and a Storyteller, Accounting her stories about the initial days of Infosys and their many failures and milestones, she left an awe-inspiring effect on the students.The great example of humility was captured by Team Iridescence."
     },
     {
-        image: "https://iiitdwd.ac.in/img/home-page/fit.jpg",
-        caption: "Teacher's Day",
-        event_caption: "You have always been an excellent educator who knew how to illuminate a soul with its light. Happy Teacher's Day to my favorite teacher!"
-    }
+        image:img3,
+        caption: "Republic Day",
+        event_caption: "Iridescence captured the Tricolor soaring high in the sky during the 74th Republic Day celebration. Respected Registrar Sir hoisted the national flag and the students put up a marvellous performance.The pride and patriotism in everyone's eyes were caught splendidly by our photography team."
+    },
+    {
+        image: img4,
+        caption: "Summer Tech Fest",
+        event_caption: "The 2-month long fest conducted by all 10 clubs of IIIT Dharwad was entirely covered by the Iridescence team. The fest brought a lot of opportunities for students to upgrade their skills and to test them with its 22 events including several workshops and contests."
+    },
+    {
+        image: img5,
+        caption: "Garba night",
+        event_caption: "It’s not about how you dress, it’s about how you dance. The speakers commanding the students to just move to the tunes and get lost into the rhythmic dandiya beats forgetting about everything. The fun and frolic event of Garba night marked a remarkable day at IIIT Dharwad and was gracefully captured by the Iridescence team."
+    },
+    {
+        image: img6,
+        caption: "Teachers day",
+        event_caption: "A feeble attempt to show our love and gratitude towards our teachers through melodies, acts and steps was done by the students of IIIT Dharwad. An evening, filled with laughter, fun and reminiscing past experiences glimpsed with the team Iridescence."
+    },
 ]
