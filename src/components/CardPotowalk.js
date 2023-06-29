8d633709a365f0c4015e9fcb911be65f6251a845
Ashith Shetty <ashitdshetty1101@gmail.com>
2023-06-29 12:39:20 +0530

    changes hover in clique section

--- src/components/CardPotowalk.js
+++ src/components/CardPotowalk.js
@@ -1,26 +1,24 @@
 import React, { useState } from 'react';
 import '../styles/Card.css';
-
 import lcliq from "../Photo/lowDSC_5249 (1).jpeg"
-
 
 const CardPotowalk = () => {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
     <div
-      className="card"
+      className={`card ${isHovered ? 'hovered' : ''}`}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
       <img src={lcliq} alt='cliqevent' className="card-img" />
-      {isHovered && (
-        <div className="card-overlay">
-          <p className="card-text">
-            Club activity where participants go out to a designated location and take photographs. The goal of a photowalk is to explore new areas, practice photography skills, and meet new people who share a passion for photography
-          </p>
-        </div>
-      )}
+      <div className="card-overlay">
+        <p className={`card-text ${isHovered ? 'raised' : ''}`}>
+        Club activity where participants go out to a designated location and take photographs. The goal of a photowalk is to explore new areas, practice photography skills, and meet new people who share a passion for photography.
+        </p>
+        {isHovered && <button className="know-more-button">Know More</button>}
+      </div>
+      
     </div>
   );
 };
--- src/components/CardWorkshop.js
+++ src/components/CardWorkshop.js
@@ -1,27 +1,25 @@
 import React, { useState } from 'react';
 import '../styles/Card.css';
+import lcliq from "../Photo/lowDSC_5256.jpeg";
 
-import lcliq from "../Photo/lowDSC_5256.jpeg"
-
-const CardWorkshop = () => {
+const CardPotowalk = () => {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
     <div
-      className="card"
+      className={`card ${isHovered ? 'hovered' : ''}`}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
       <img src={lcliq} alt='cliqevent' className="card-img" />
-      {isHovered && (
-        <div className="card-overlay">
-          <p className="card-text">
-          In club comprehensive photography workshop aimed at improving skills in using a DSLR camera. This workshop covered the basics of photography and provide hands-on training on how to effectively use a DSLR camera. 
-          </p>
-        </div>
-      )}
+      <div className="card-overlay">
+        <p className={`card-text ${isHovered ? 'raised' : ''}`}>
+          In club comprehensive photography workshop aimed at improving skills in using a DSLR camera. This workshop covered the basics of photography and provide hands-on training on how to effectively use a DSLR camera.
+        </p>
+        {isHovered && <button className="know-more-button">Know More</button>}
+      </div>
     </div>
   );
 };
 
-export default CardWorkshop;
+export default CardPotowalk;
