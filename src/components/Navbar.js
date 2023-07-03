b7c133663aa7623be748e9c1b21245e53a351784
Ashith Shetty <ashitdshetty1101@gmail.com>
2023-07-03 18:40:57 +0530

    fixed bug

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,6 +16,12 @@ function Navbar() {
     }
       
   };
+  const change=()=>{
+    if(window.location.pathname==='/'){
+      window.location.pathname="/#"
+    }
+
+  }
 
   const menuBtn = useRef(null)
   
@@ -36,7 +42,7 @@ function Navbar() {
         <Link to='/lensation' className='topnav-link' onClick={handleLinkClick}>Lensation</Link>
         <Link to='/team' className='topnav-link' onClick={handleLinkClick}>Team</Link>
         <Link to='/work' className='topnav-link'  onClick={handleLinkClick}>Work</Link>
-        <a href="/" className='dropdown-nav icon' onClick={handleClick}>   
+        <a href={change} className='dropdown-nav icon' onClick={handleClick}>    
             <FiMenu size={20}/>
         </a>
 
