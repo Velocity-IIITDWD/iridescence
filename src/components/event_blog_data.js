4d486689b8ca19fc6d49a353b31e162aaa1f33e1
Ashith Shetty <ashitdshetty1101@gmail.com>
2023-07-10 00:34:05 +0530

    added few images

--- src/components/event_blog_data.js
+++ src/components/event_blog_data.js
@@ -1,31 +1,38 @@
-import img2 from "../Work Photo/DSC_6265.jpg"
-import img3 from "../Work Photo/DSC_0066.jpg"
-import img4 from "../Work Photo/DSC_3172.jpg"
-import img5 from "../Work Photo/IMG_0430.jpg"
-import img6 from "../Work Photo/DSC_0112.jpg"
+import img2 from "../Work_Photo/DSC_0066.jpg"
+import img3 from "../Work_Photo//DSC_0112.jpg"
+import img4 from "../Work_Photo/DSC_3172.jpg"
+
+
+import img5 from "../Work_Photo/DSC_6265.jpg"
+import img7 from "../Work_Photo/DSC_0141.JPG"
+import img8 from "../Work_Photo/DSC_5926.JPG"
+import img9 from "../Work_Photo/DSC_0328.JPG"
+
+import img6 from "../Work_Photo/IMG_0430.jpg"
+
 export const event_blog_data = [
     {
-        image: [img2,img5,img4,img5],
+        image: [img5,img7,img8,img9],
         caption: "Smt.Sudha Murthy Mam's visit",
         event_caption: "The Chairperson of IIIT Dharwad, on her visit to the campus, sat down among the students to have a warm conversation. Being a Philanthropist and a Storyteller, Accounting her stories about the initial days of Infosys and their many failures and milestones, she left an awe-inspiring effect on the students.The great example of humility was captured by Team Iridescence."
     },
     {
-        image:[img3,img2,img4,img5],
+        image:[img2,img2,img2,img2],
         caption: "Republic Day",
         event_caption: "Iridescence captured the Tricolor soaring high in the sky during the 74th Republic Day celebration. Respected Registrar Sir hoisted the national flag and the students put up a marvellous performance.The pride and patriotism in everyone's eyes were caught splendidly by our photography team."
     },
     {
-        image: [img4,img3,img2,img5],
+        image: [img4,img4,img4,img4],
         caption: "Summer Tech Fest",
         event_caption: "The 2-month long fest conducted by all 10 clubs of IIIT Dharwad was entirely covered by the Iridescence team. The fest brought a lot of opportunities for students to upgrade their skills and to test them with its 22 events including several workshops and contests."
     },
     {
-        image: [img5,img3,img4,img2],
+        image: [img6,img6,img6,img6],
         caption: "Garba night",
         event_caption: "It’s not about how you dress, it’s about how you dance. The speakers commanding the students to just move to the tunes and get lost into the rhythmic dandiya beats forgetting about everything. The fun and frolic event of Garba night marked a remarkable day at IIIT Dharwad and was gracefully captured by the Iridescence team."
     },
     {
-        image:[img6,img3,img4,img5],
+        image:[img3,img3,img3,img3],
         caption: "Teachers day",
         event_caption: "A feeble attempt to show our love and gratitude towards our teachers through melodies, acts and steps was done by the students of IIIT Dharwad. An evening, filled with laughter, fun and reminiscing past experiences glimpsed with the team Iridescence."
     },
