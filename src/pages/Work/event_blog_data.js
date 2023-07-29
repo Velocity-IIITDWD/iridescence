220604fe35a12fb58c52fbd8b5427f5110d1e51e
ChaytaliJ <csjawalekar@gmail.com>
2023-07-29 23:11:00 +0530

    Data addition to few events along with images

--- src/pages/Work/event_blog_data.js
+++ src/pages/Work/event_blog_data.js
@@ -8,6 +8,19 @@ import img9 from "../../ImagesFolder/Work_Photo/DSC_0328.JPG"
 
 import img6 from "../../ImagesFolder/Work_Photo/IMG_0430.jpg"
 
+import img10 from "../../ImagesFolder/Work_Photo/sports_5.jpg"
+import img11 from "../../ImagesFolder/Work_Photo/sports_4.jpg"
+import img12 from "../../ImagesFolder/Work_Photo/sports_3.jpg"
+import img13 from "../../ImagesFolder/Work_Photo/sports_2.jpg"
+import img14 from "../../ImagesFolder/Work_Photo/sports_1.jpg"
+
+
+
+import img15 from "../../ImagesFolder/Work_Photo/convocation_4.jpg"
+import img16 from "../../ImagesFolder/Work_Photo/convocation_3.jpg"
+import img17 from "../../ImagesFolder/Work_Photo/convocation_2.jpg"
+import img18 from "../../ImagesFolder/Work_Photo/convocation_1.jpg"
+
 export const event_blog_data = [
     {
         image: [img5,img7,img8,img9],
@@ -34,4 +47,25 @@ export const event_blog_data = [
         caption: "Teachers day",
         event_caption: "A feeble attempt to show our love and gratitude towards our teachers through melodies, acts and steps was done by the students of IIIT Dharwad. An evening, filled with laughter, fun and reminiscing past experiences glimpsed with the team Iridescence."
     },
+    {
+        image:[img10,img11,img12,img13,img14],
+        caption: "Sports day",
+        event_caption: "\"Champions on the Field, marking the Sports day with the spirit of competitionand camaraderie.\"The air was charged with excitement as they pushed their limits, fueled by passion and determination.Beyond the trophies and medals, the true essence of the event lay in the values of unity, respect, and fair play that prevailed throughout. As the sun set on the day, leaving behind memories of joy and inspiration, the Iridescence, through their lenses captured the vibrant colors of team jerseys, the intensity in athletes' eyes, and the jubilation of victory."
+    },
+    {
+        image:[],
+        caption: "Independence day",
+        event_caption: "The 75th Independence Day of India at IIIT Dharwad was marked by not only the usual patriotic fervor but also unique initiatives like \"Shramadhan\" and \"Har Ghar Tiranga.\" \"Shramadhan\" witnessed citizens coming together to engage in community service, epitomizing the spirit of selfless dedication towards the betterment of society. The \"Har Ghar Tiranga\" campaign adorned every home with the national flag, reflecting a sense of pride and unity that extended to every nook and corner of the country. Iridescence beautifully documented the essence of these meaningful endeavors, showcasing the collective efforts towards a stronger and more inclusive India with their commendable photographs."
+    },
+    {
+        image:[img15,img16,img17,img18],
+        caption: "Convocation day",
+        event_caption: ""
+    },
+    {
+        image:[],
+        caption: "Inter IIIT",
+        event_caption: "\"Inter IIIT Sports: Fueling the Spirit of Excellence and Sporting Unity!.\"The sports competitions at Inter IIIT cover a diverse array of disciplines, catering to athletes with different interests and strengths. From track and field events such as sprints, long jumps, and shot put to team sports like cricket, football, basketball, and volleyball, the sports arena was alive with passion and determination.Some of the beautiful moments were captured by Iridescence."
+    },
+
 ]
